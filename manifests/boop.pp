07c8078ca31a80818598bb8c5d17f5a0f561f33a
Eric Johnson <eric.git@iijo.org>
2012-12-15 23:49:49 +0000

    varnish and aliases.

---
+++ manifests/boop.pp
@@ -0,0 +1,16 @@
+file {'aliases':
+  path    => '/home/eric/.aliases',
+  ensure  => present,
+  owner   => 'eric',
+  group   => 'eric',
+  mode    => 0644,
+  source  => '/home/eric/code/puppet/files/home/eric/.aliases'
+}
+file {'varnish':
+  path    => '/etc/varnish/default.vcl',
+  ensure  => present,
+  owner   => 'root',
+  group   => 'root',
+  mode    => 0644,
+  source  => '/home/eric/code/puppet/files/etc/varnish/default.vcl'
+}
