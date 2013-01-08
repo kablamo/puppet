3bf08b03dadd8dc304506babc51d75ca4a630f29
Eric Johnson <eric.git@iijo.org>
2013-01-08 21:10:24 +0000

    require perlbrew.

--- modules/kablamo/manifests/perl.pp
+++ modules/kablamo/manifests/perl.pp
@@ -1,18 +1,19 @@
 class kablamo::perl ($version) {
     require kablamo::packages
+    require perlbrew
 
-    # packages needed to build stuff
+    # packages needed to build modules
     package { 
         'libssl-dev':           ensure => present; # for https
         'zlib1g-dev':           ensure => present; # for gzip
         'libxml2-dev':          ensure => present; # for XML::Parser
         'libexpat1-dev':        ensure => present; # for XML::Parser
         'libcurl4-openssl-dev': ensure => present; # AnyEvent::Curl::Multi
-    }->
- 
+    }
+
     # install the perl
-    perlbrew::build { $version: }->
+    perlbrew::build { $version: version => $version }->
 
     # install cpanm
-    perlbrew::install_cpanm { $version: } } }   
+    perlbrew::install_cpanm { $version: version => $version }
 }
