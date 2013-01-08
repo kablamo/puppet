35c5a69b20ebd6535514aa2b81ec1b02554b6acd
Eric Johnson <eric.git@iijo.org>
2013-01-08 21:11:04 +0000

    Exec blocks need user, group and updatec command to something valid.

--- modules/perlbrew/manifests/init.pp
+++ modules/perlbrew/manifests/init.pp
@@ -1,23 +1,27 @@
-class perlbrew ($version = $name) {
+class perlbrew {
   include perlbrew::params
   include perlbrew::install
 
-  define build ($version) {
+  define build ($version = $name) {
     exec { "perlbrew_build_${name}":
-        command => "PERLBREW_ROOT=${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install ${version} --as ${name} -Accflags=-fPIC -Dcccdlflags=-fPIC'",
+        command => "/bin/bash -c 'PERLBREW_ROOT=${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install ${version} --as ${name} -Accflags=-fPIC -Dcccdlflags=-fPIC'",
         timeout => 3600,
+        user    => 'eric',
+        group   => 'eric',
         creates => "${perlbrew::params::perlbrew_root}/perls/${name}",
-        require => Class["perlbrew::environment"],
+        require => [Class["perlbrew::params"], Class["perlbrew::install"]],
     }
   }
 
-  define install_cpanm () {
+  define install_cpanm ($version = $name) {
     exec { "install_cpanm_${name}":
         # puppet seems to change the current user weirdly when using the
         # user/group options. That causes cpanm to use /root/.cpanm for it's
         # temporary storage, which happens to not be writable for the perlbrew
         # user. Use /bin/su to work this around.
-        command => "${perlbrew::params::perlbrew_bin} install-cpanm",
+        command => "/bin/bash -c 'PERLBREW_ROOT='${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install-cpanm",
+        user    => 'eric',
+        group   => 'eric',
         creates => "${perlbrew::params::perlbrew_root}/perls/${name}/bin/cpanm",
         require => Perlbrew::Build[$name],
     }
