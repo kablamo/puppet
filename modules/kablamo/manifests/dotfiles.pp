e3dcd362cb5a42e996947119830175297c5f505a
Eric Johnson <eric.git@iijo.org>
2013-01-08 21:08:40 +0000

    Added user and group attributes to the exec blocks.

--- modules/kablamo/manifests/dotfiles.pp
+++ modules/kablamo/manifests/dotfiles.pp
@@ -3,11 +3,15 @@ class kablamo::dotfiles {
 
     exec { clone-dotfiles:
         command => '/usr/bin/git clone https://github.com/kablamo/dotfiles.git /home/eric/code/dotfiles',
+        user    => 'eric',
+        group   => 'eric',
         creates => '/home/eric/code/dotfiles'
     }
 
     exec { setup-dotfiles:
         command => '/home/eric/code/dotfiles/bin/dotfiles',
+        user    => 'eric',
+        group   => 'eric',
         creates => '/home/eric/.tmux.conf',
     }
 }
