2c13579cecdebb65d641d6a20e0b9ecdc993e6b9
Eric Johnson <eric.git@iijo.org>
2013-01-08 21:12:01 +0000

    Puppet can't expand ~ to my home dir.

--- modules/perlbrew/manifests/params.pp
+++ modules/perlbrew/manifests/params.pp
@@ -1,5 +1,5 @@
 class perlbrew::params {
-  $perlbrew_root = "~/perl5/perlbrew"
-  $perlbrew_bin  = "~/perl5/perlbrew/bin/perlbrew"
+  $perlbrew_root = "/home/eric/perl5/perlbrew"
+  $perlbrew_bin  = "/home/eric/perl5/perlbrew/bin/perlbrew"
   $cpanm_url     = "http://github.com/miyagawa/cpanminus/raw/master/cpanm"
 }
