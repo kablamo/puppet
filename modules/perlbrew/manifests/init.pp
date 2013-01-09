ec2daf39e9667b2ab7e7454333d04ee3841f66cf
Eric Johnson <eric.git@iijo.org>
2013-01-09 09:00:12 +0000

    Removed $name.  Added $user, $group.

--- modules/perlbrew/manifests/init.pp
+++ modules/perlbrew/manifests/init.pp
@@ -1,29 +1,29 @@
-class perlbrew {
+class perlbrew (user, group) {
   include perlbrew::params
   include perlbrew::install
 
-  define build ($version = $name) {
-    exec { "perlbrew_build_${name}":
-        command => "/bin/bash -c 'PERLBREW_ROOT=${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install ${version} --as ${name} -Accflags=-fPIC -Dcccdlflags=-fPIC'",
+  define build ($version) {
+    exec { "perlbrew_build_${version}":
+        command => "/bin/bash -c 'PERLBREW_ROOT=${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install ${version} --as ${version} -Accflags=-fPIC -Dcccdlflags=-fPIC'",
         timeout => 3600,
-        user    => 'eric',
-        group   => 'eric',
-        creates => "${perlbrew::params::perlbrew_root}/perls/${name}",
+        user    => $user,
+        group   => $group,
+        creates => "${perlbrew::params::perlbrew_root}/perls/${version}",
         require => [Class["perlbrew::params"], Class["perlbrew::install"]],
     }
   }
 
-  define install_cpanm ($version = $name) {
-    exec { "install_cpanm_${name}":
+  define install_cpanm ($version) {
+    exec { "install_cpanm_${version}":
         # puppet seems to change the current user weirdly when using the
         # user/group options. That causes cpanm to use /root/.cpanm for it's
         # temporary storage, which happens to not be writable for the perlbrew
         # user. Use /bin/su to work this around.
         command => "/bin/bash -c 'PERLBREW_ROOT='${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install-cpanm",
-        user    => 'eric',
-        group   => 'eric',
-        creates => "${perlbrew::params::perlbrew_root}/perls/${name}/bin/cpanm",
-        require => Perlbrew::Build[$name],
+        user    => $user,
+        group   => $group,
+        creates => "${perlbrew::params::perlbrew_root}/perls/${version}/bin/cpanm",
+        require => Perlbrew::Build[$version],
     }
   }
 }
