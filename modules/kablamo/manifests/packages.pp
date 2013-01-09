02822e8a728dc78f325f360333070757b9e4240c
Eric Johnson <eric.git@iijo.org>
2013-01-09 23:22:38 +0000

    Move perl module packages in here.

--- modules/kablamo/manifests/packages.pp
+++ modules/kablamo/manifests/packages.pp
@@ -4,9 +4,13 @@ class kablamo::packages {
     package { curl:            ensure => latest }
     package { git:             ensure => latest }
     package { htop:            ensure => latest }
+    package { libexpat1-dev:   ensure => latest } # XML::Parser
+    package { libssl-dev:      ensure => latest }
+    package { libxml2-dev:     ensure => latest } # XML::Parser
     package { locate:          ensure => latest }
     package { mtr:             ensure => latest }
     package { nano:            ensure => absent }
+    package { ntp:             ensure => latest }
     package { sqlite3-doc:     ensure => latest }
     package { sqlite3:         ensure => latest }
     package { sshfs:           ensure => latest }
@@ -14,7 +18,7 @@ class kablamo::packages {
     package { varnish:         ensure => latest }
     package { vim-nox:         ensure => latest }
     package { wget:            ensure => latest }
-    package { ntp:             ensure => latest }
+    package { zlib1g-dev:      ensure => latest } # gzip lib
 
     exec { "initialize-locate-db":
         command     => "updatedb",
