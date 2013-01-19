3a682037ef4d19968943bfc811f1be390cda2996
Eric Johnson <eric.git@iijo.org>
2013-01-19 22:44:15 +0000

    Minor change.  Just a bit neater.

--- modules/kablamo/manifests/perl.pp
+++ modules/kablamo/manifests/perl.pp
@@ -1,6 +1,8 @@
 class kablamo::perl ($perlbrew_root, $user, $version) {
-
-    class { 'perlbrew': user => $user, perlbrew_root => $perlbrew_root }
-    perlbrew::install_perl  { $version: version => $kablamo::perl_version }->
-    perlbrew::install_cpanm { $version: version => $kablamo::perl_version }
+    class { 'perlbrew': 
+        user          => $user, 
+        perlbrew_root => $perlbrew_root 
+    }
+    perlbrew::install_perl  { $version: version => $version}
+    perlbrew::install_cpanm { $version: version => $version}
 }
