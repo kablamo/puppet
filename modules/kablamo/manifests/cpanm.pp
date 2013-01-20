1e72b80b44de5fd3fc62adaa094e032eff629cac
Eric Johnson <eric.git@iijo.org>
2013-01-20 17:32:11 +0000

    Make it better for more libs.

--- modules/kablamo/manifests/cpanm.pp
+++ modules/kablamo/manifests/cpanm.pp
@@ -1,14 +1,18 @@
 define cpanm (
     $ensure  = 'present',
     $creates = $ensure ? {
-        'present' => regsubst("${kablamo::perl_locallib}/lib/perl5/${title}.pm", "::", "/", 'G'), 
+        'present' => [ regsubst("${kablamo::perl_locallib}/lib/perl5/${title}.pm", "::", "/", 'G'), 
+                       regsubst("${kablamo::perl_locallib}/lib/perl5/i686-linux/${title}.pm", "::", "/", 'G'), 
+                       regsubst("${kablamo::perl_locallib}/lib/5.16.2/${title}.pm", "::", "/", 'G'), 
+                       regsubst("${kablamo::perl_locallib}/lib/5.16.2/i686-linux/${title}.pm", "::", "/", 'G'), 
+                     ],
         default   => '/asdfaevdsdfdfevevdf923',
     }
 ) {
     require kablamo::perl
 
     exec { "cpanm $title":
-        command   => "${kablamo::perlbrew_root}/bin/cpanm --local-lib-contained=${kablamo::perl_locallib} $title",
+        command   => "${kablamo::perl_locallib}/bin/perl ${kablamo::perlbrew_root}/bin/cpanm --local-lib-contained=${kablamo::perl_locallib} $title",
         user      => $kablamo::user,
         group     => $kablamo::user,
         logoutput => true,
