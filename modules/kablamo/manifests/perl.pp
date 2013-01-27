8f533a57be9a580bf89481c3ac383fb84e185acf
Eric Johnson <eric.git@iijo.org>
2013-01-27 13:27:13 +0000

    Include switch

--- modules/kablamo/manifests/perl.pp
+++ modules/kablamo/manifests/perl.pp
@@ -4,5 +4,6 @@ class kablamo::perl ($perlbrew_root, $user, $version) {
         perlbrew_root => $perlbrew_root 
     }
     perlbrew::install_perl  { $version: version => $version}
+    perlbrew::switch        { $version: version => $version}
     perlbrew::install_cpanm { $version: version => $version}
 }
