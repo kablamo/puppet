ebc9d1b09cf1e6eb95e3805093e52f9aebe08e0a
Eric Johnson <eric.git@iijo.org>
2013-02-06 06:59:59 +0000

    Added exim barebones.  Still need configuration.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node eric, 'kablamo.xen.prgmr.com' {
     include kablamo
     include varnish
+    include exim
 }
 node default {
     include kablamo
---
+++ modules/exim/manifests/init.pp
@@ -0,0 +1,25 @@
+class exim {
+    $files="${settings::confdir}/modules/exim/files"
+
+#    file { '/etc/varnish':
+#        ensure => 'directory',
+#        owner  => 'root',
+#        group  => 'root',
+#        mode   => '0755',
+#    }
+#    file { '/etc/varnish/default.vcl':
+#        ensure => 'file',
+#        owner  => 'root',
+#        group  => 'root',
+#        mode   => '0644',
+#        source => "${files}/etc/varnish/default.vcl",
+#    }
+
+    package { exim4: ensure => $ensure }
+    service { exim4_service: 
+        name     => 'exim4',
+        enable   => true,
+        ensure   => true, 
+        require  => Package["exim4"],
+    }
+}
