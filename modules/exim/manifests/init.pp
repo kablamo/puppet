04e713c1ed096dde361f19f89baa7dcf2bcc430f
Eric Johnson <eric.git@iijo.org>
2013-02-07 01:04:57 +0000

    Update exim config files.

--- modules/exim/manifests/init.pp
+++ modules/exim/manifests/init.pp
@@ -1,19 +1,20 @@
 class exim {
     $files="${settings::confdir}/modules/exim/files"
 
-#    file { '/etc/varnish':
-#        ensure => 'directory',
-#        owner  => 'root',
-#        group  => 'root',
-#        mode   => '0755',
-#    }
-#    file { '/etc/varnish/default.vcl':
-#        ensure => 'file',
-#        owner  => 'root',
-#        group  => 'root',
-#        mode   => '0644',
-#        source => "${files}/etc/varnish/default.vcl",
-#    }
+    file { '/etc/exim4/update-exim4.conf.conf':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "${files}/etc/exim4/update-exim4.conf.conf",
+    }
+    file { '/etc/exim4/router/350_exim4-config_domain_redirect':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "${files}/etc/exim4/router/350_exim4-config_domain_redirect",
+    }
 
     package { exim4: ensure => $ensure }
     package { mailutils: ensure => $ensure }
