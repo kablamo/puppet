287f1f08c2876479febecd648f71b7193f3d96ec
Eric Johnson <eric.git@iijo.org>
2013-02-08 10:09:59 +0000

    Put in the config files.

--- modules/dovecot/manifests/init.pp
+++ modules/dovecot/manifests/init.pp
@@ -1,19 +1,26 @@
 class dovecot {
     $files="${settings::confdir}/modules/dovecot/files"
 
-    #file { '/etc/dovecot':
-    #    ensure => 'directory',
-    #    owner  => 'root',
-    #    group  => 'root',
-    #    mode   => '0755',
-    #}
-    #file { '/etc/dovecot/dovecot.conf':
-    #    ensure => 'file',
-    #    owner  => 'root',
-    #    group  => 'root',
-    #    mode   => '0644',
-    #    source => "${files}/etc/dovecot/dovecot.conf",
-    #}
+    file { '/etc/dovecot':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+    file { '/etc/dovecot/conf.d/10-ssl.conf':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "${files}/etc/dovecot/conf.d/10-ssl.conf",
+    }
+    file { '/etc/dovecot/conf.d/10-mail.conf':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "${files}/etc/dovecot/conf.d/10-mail.conf",
+    }
 
     package { dovecot-pop3d: ensure => 'present' }
     service { dovecot_service: 
