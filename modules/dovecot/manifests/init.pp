73a6909a67540714002b3cfa8178f784ac1d3971
Eric Johnson <eric.git@iijo.org>
2013-02-08 10:12:29 +0800

    Dovecot

---
+++ modules/dovecot/manifests/init.pp
@@ -0,0 +1,25 @@
+class dovecot {
+    $files="${settings::confdir}/modules/dovecot/files"
+
+    #file { '/etc/dovecot':
+    #    ensure => 'directory',
+    #    owner  => 'root',
+    #    group  => 'root',
+    #    mode   => '0755',
+    #}
+    #file { '/etc/dovecot/dovecot.conf':
+    #    ensure => 'file',
+    #    owner  => 'root',
+    #    group  => 'root',
+    #    mode   => '0644',
+    #    source => "${files}/etc/dovecot/dovecot.conf",
+    #}
+
+    package { dovecot: ensure => 'present' }
+    service { dovecot_service: 
+        name     => 'dovecot',
+        enable   => true,
+        ensure   => true, 
+        require  => Package["dovecot"],
+    }
+}
