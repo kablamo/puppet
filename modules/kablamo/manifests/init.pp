d7a7e4ecf1ce397eefa9cb1bd4e78634f834025e
Eric Johnson <eric.git@iijo.org>
2013-02-08 02:23:09 +0000

    Removed some comments.

--- modules/kablamo/manifests/init.pp
+++ modules/kablamo/manifests/init.pp
@@ -27,14 +27,6 @@ class kablamo {
     #  mode    => 0644,
     #  source  => 'puppet:///files/home/eric/.aliases'
     #}
-    #file { varnish:
-    #  path    => '/etc/varnish/default.vcl',
-    #  ensure  => present,
-    #  owner   => 'root',
-    #  group   => 'root',
-    #  mode    => 0644,
-    #  source  => 'puppet:///files/etc/varnish/default.vcl'
-    #}
     #exec { npm:
     #    command => '/usr/bin/curl http://npmjs.org/install.sh | sh',
     #    creates => '/some/directory/somewhere'
