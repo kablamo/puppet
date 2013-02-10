264ecca3f8a39fd665ae147bf93c77ef14039ce6
Eric Johnson <eric.git@iijo.org>
2013-02-10 01:39:44 +0000

    .

--- modules/perlbal/manifests/init.pp
+++ modules/perlbal/manifests/init.pp
@@ -2,5 +2,6 @@ class perlbal {
     require kablamo::perl
     cpanm { 'Perlbal':                            ensure => $ensure }
     cpanm { 'Perlbal::XS::HTTPHeaders':           ensure => $ensure }
+    cpanm { 'Perlbal::Plugin::SessionAffinity':   ensure => $ensure }
     cpanm { 'IO::AIO':                            ensure => $ensure }
 }
