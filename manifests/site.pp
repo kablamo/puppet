eb71f1027e95ceeba6d464f89aad64693ac61afb
Eric Johnson <eric.git@iijo.org>
2013-02-15 23:40:26 +0800

    Add cron backup jobs

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,27 @@ node 'kablamo.xen.prgmr.com' {
     include exim
     include dovecot
     include nginx
+
+    cron { monthly_backup:
+        command  => '/home/eric/code/networthify.com/script/backup monthly',
+        user     => 'eric',
+        monthday => 1,
+        hour     => 1,
+        minute   => 0,
+    }
+    cron { weekly_backup:
+        command => '/home/eric/code/networthify.com/script/backup weekly',
+        user    => 'eric',
+        weekday => 0,
+        hour    => 2,
+        minute  => 0,
+    }
+    cron { daily_backup:
+        command => '/home/eric/code/networthify.com/script/backup daily',
+        user    => 'eric',
+        hour    => 3,
+        minute  => 0,
+    }
 }
 node default {
     include kablamo
